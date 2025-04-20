c86d50b375e3962d939e97e6228ea5824a289f28
njohndev <njohn.developer@gmail.com>
2025-04-21 02:10:45 +1000

    Replace device_pm_control_nop with NULL

--- app/src/behaviors/behavior_trackball_pim447.c
+++ app/src/behaviors/behavior_trackball_pim447.c
@@ -50,8 +50,8 @@ static const struct behavior_driver_api behavior_trackball_pim447_driver_api = {
     static const struct config config_##n = {.mode = DT_INST_PROP(n, mode),                        \
                                              .momentary = DT_INST_PROP(n, momentary)};             \
                                                                                                    \
-    DEVICE_DT_INST_DEFINE(n, behavior_trackball_pim447_init, device_pm_control_nop, NULL,          \
-                          &config_##n, APPLICATION, CONFIG_KERNEL_INIT_PRIORITY_DEFAULT,           \
+    DEVICE_DT_INST_DEFINE(n, behavior_trackball_pim447_init, NULL, NULL, &config_##n, APPLICATION, \
+                          CONFIG_KERNEL_INIT_PRIORITY_DEFAULT,                                     \
                           &behavior_trackball_pim447_driver_api);
 
 DT_INST_FOREACH_STATUS_OKAY(PIM447_INST)
